3f1f3bea21b806c0cd370b0a6de90af82e936427
rahwa <rahwaraya@gmail.com>
2023-01-13 16:51:48 -0600

    on click listener was add for signin

--- app/src/main/java/com/emonics/farmersapp/Home.kt
+++ app/src/main/java/com/emonics/farmersapp/Home.kt
@@ -1,11 +1,33 @@
 package com.emonics.farmersapp
 
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        // get reference to textview
+        // val sign_in= findViewById(R.id.Sign_in)
+
+        val signin= findViewById<TextView>(R.id.Sign_in)
+        // crating on click listener for the textview
+        // set on-click listener
+        signin.setOnClickListener {
+            val Intent= Intent(this,Home:: class.java )
+            startActivity(Intent)
+        }
+        // for the items
+        val freshfood= findViewById (R.id.fresh_food)as ImageView
+        // set on-click listener
+        signin.setOnClickListener {
+            val Intent= Intent(this,page1:: class.java )
+            startActivity(Intent)
+        }
     }
 }
---
+++ app/src/main/java/com/emonics/farmersapp/page1.kt
@@ -0,0 +1,11 @@
+package com.emonics.farmersapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class page1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page1)
+    }
+}
