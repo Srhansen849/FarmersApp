b32893275b6f3342e7c93f36ee78be638d40827a
bhavyadesai12 <69489809+bhavyadesai12@users.noreply.github.com>
2023-01-22 18:42:52 -0800

    LoginActivity.kt

--- app/src/main/java/com/emonics/farmersapp/MainActivity.kt
+++ app/src/main/java/com/emonics/farmersapp/MainActivity.kt
@@ -7,5 +7,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageId = intArrayOf(
+            R.drawable.apple,R.drawable.banana,R.drawable.dragonfruit,R.drawable.grapes,R.drawable.kiwi,
+            R.drawable.mango
+        )
+
+        val name = arrayOf(
+            "Apple",
+            "Banana",
+            "Dragonfruit",
+            "Grapes",
+            "Kiwi"
+            "Mango"
+        )
+
+        val itemDescription = arrayOf(
+            "Fresh apple","Fresh banana","Fresh","Fresh Dragonfruit","Fresh kiwi",
+        "Fresh mango"
+        )
+
+
     }
 }
