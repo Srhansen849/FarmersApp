0073bc8d645dd845b35390dfa56715af570d621e
bhavyadesai12 <69489809+bhavyadesai12@users.noreply.github.com>
2023-01-24 07:58:47 -0800

    LoginActivity.kt

--- app/src/main/java/com/emonics/farmersapp/UsersAPI.kt
+++ app/src/main/java/com/emonics/farmersapp/UsersAPI.kt
@@ -3,7 +3,7 @@ package com.emonics.farmersapp
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import android
 
 @Dao
 interface UsersAPI {
